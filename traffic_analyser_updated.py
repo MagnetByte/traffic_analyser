ab3872ee0bc8fd513be2780516f6d274fef33b56
Dhananjay Maurya <dhananjaym182@gmail.com>
2025-04-26 21:09:41 +0530

    Update traffic_analyser_updated.py

--- traffic_analyser_updated.py
+++ traffic_analyser_updated.py
@@ -689,7 +689,7 @@ class GetData(object):
             request_no = opts.compact[1]
 
         # Limit geo api to 30 ip's to avoid potential abuse.
-        if isinstance(ip_no, int) 
+        if not opts.nogeo:
             if ip_no > 30:
                 self.data['geo_limit'] = ip_no
                 ip_no = 30
